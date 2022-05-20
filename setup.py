8a43f824bde9465bdd40a618bfd264d7659c2e95
Nico Voigt <nico.jsgww@gmx.net>
2022-05-20 20:28:17 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='kaggle_kore',
+    version='0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='nico voigt',
+    author_email='nico.jsgww@gmx.net',
+    description='Package for the Kaggle Kore competition'
+)
