a6f39c6e30055c3c26c68759440610866ad8c5ff
Nico Voigt <nico.jsgww@gmx.net>
2022-05-25 23:15:06 +0200

    multi-agent seems to work now

--- kaggle_kore/RL_Controller/own/agent.py
+++ kaggle_kore/RL_Controller/own/agent.py
@@ -94,103 +94,142 @@ def attack_opponent_base(board, idx):
 class Controller:
     def __init__(self) -> None:
         self.total_train_counter = 0
-        self.rl_state = None
-        self.last_rl_state = None
+        self.rl_state = dict()
+        self.last_rl_state = dict()
         self.rl_names = ["turn", "kore_left", "kore_opp",  "max_spawn", "ships_in_shipyard", "kore_of_fleet", "num_shipyards" ,"n_opp_shipyards" ,"done"]
         self.state_size = len(self.rl_names)
-        self.last_rl_action = None
-        self.agent = Agent(self.state_size, action_size=4, lr=0.005)
+        self.last_rl_action = dict()
+        self.agents = [Agent(self.state_size, action_size=4, lr=0.005)]
+        self.obs = dict()
+        self.config = dict()
+        self.me = dict()
+        self.num_shipyards = 1
+    
+    def get_states(self, obs, config, me, idx):
+        board, me, turn, spawn_cost, kore_left, max_spawn, kore_opp, opp_shipyards, num_shipyards = unbundle_stuff(obs, config)
+        
+        new_shipyards = num_shipyards - self.num_shipyards
+        if (turn >0) and (new_shipyards > 0) and (idx >=idx):
+            for sy in range(new_shipyards):
+                self.create_new_agent(idx)
+        self.obs[idx] = obs
+        self.config[idx] = config
+        self.me[idx] = me
+        self.num_shipyards = num_shipyards
 
-    def get_states(self, obs, config):
-        self.obs = obs
-        self.config = config
+        # TOD hier checken, ob es ein neues shipyard gibt. Wenn ja, dann baue ein neues.
 
-    def store_transition(self, state, action, reward, next_state, done):
-        self.agent.store_transition(state, action, reward, next_state, done)
+
+    def create_new_agent(self, idx) -> None:
+        print("new base built")
+        #TODO  Am besten hier später eine Kopie einfügen, die pretrained ist.Oder den gleichen noch einmal reinkopieren
+
+        # es gibt schon eine neue base, aber zuerst werden die anderen gesteuert
+        # also abfragen, ob schon die neue base angesprochen wird
+        next_idx = len(self.agents) - idx
+        self.agents.append(Agent(self.state_size, action_size=4, lr=0.005))
+        self.rl_state[idx +1] = self.rl_state[idx]
+
+    def store_transition(self, state, action, reward, next_state, done, idx):
+        self.agents[idx].store_transition(state, action, reward, next_state, done)
         
 
-    def map_input_to_rl_state(self, obs, config):
+    def map_input_to_rl_state(self, obs, config, idx):
         done = 0
         board, me, turn, spawn_cost, kore_left, max_spawn, kore_opp, opp_shipyards, num_shipyards = unbundle_stuff(obs, config)
         if (turn == 399) or (opp_shipyards==0):
             done = 1
-        ships_in_shipyard = me.shipyards[0].ship_count
+        ships_in_shipyard = me.shipyards[idx].ship_count
         kore_of_fleet = sum([fleet.kore for fleet in me.fleets])
         rl_state = [turn, kore_left, kore_opp, max_spawn, ships_in_shipyard, 
                     kore_of_fleet, num_shipyards, opp_shipyards, done]
         return rl_state
 
-    def make_transition(self, obs, config):
-        reward = self.calc_reward()
-        self.last_rl_state = self.rl_state
-        rl_state = self.map_input_to_rl_state(obs, config)
+    def make_transition(self, obs, config, idx):
+        board, me, turn, spawn_cost, kore_left, max_spawn, kore_opp, opp_shipyards, num_shipyards = unbundle_stuff(obs, config)
+
+        reward = self.calc_reward(idx)
+        if turn >0:
+            self.last_rl_state[idx] = self.rl_state[idx]
+        rl_state = self.map_input_to_rl_state(obs, config, idx)
         
-        if self.last_rl_action != None:
-            self.store_transition(self.last_rl_state, self.last_rl_action, reward, rl_state, done=rl_state[-1])
+        if len(self.last_rl_action) != 0 and len(self.last_rl_state) != len(self.rl_state):
+            self.store_transition(self.last_rl_state[idx], self.last_rl_action[idx], reward, rl_state, done=rl_state[-1], idx=idx)
         
         # noch schauen, dass kein zusammenhang zwischen den states besteht beim resetten 
         if rl_state[-1] == 1:
             pass
-        self.rl_state = rl_state
+        self.rl_state[idx] = rl_state
 
         if self.total_train_counter % 1000 == 0:
-            self.agent.train()
+            for agent in self.agents:
+                agent.train()
         
         self.total_train_counter +=1
 
         return rl_state
 
-    def choose_action(self, rl_state, obs, config, shipyard_idx):
-        action_raw = self.agent.choose_action(state_input = rl_state)
+    def choose_action(self, rl_state, obs, config, idx):
+        
+        action_raw = self.agents[idx].choose_action(state_input = rl_state)
         action_raw = np.argmax(action_raw)
         action_raw = randint(0,3)
-        action = self.map_action(action_raw,  obs, config, shipyard_idx)
+        action = self.map_action(action_raw,  obs, config, idx)
         
-        self.last_rl_action = action_raw
+        self.last_rl_action[idx] = action_raw
         return action
 
-    def calc_reward(self):
+    def calc_reward(self, idx):
         reward = 0
-        if self.last_rl_state is None:
+        if len(self.last_rl_state) == 0:        # wenn das noch nicht gefüllt wurde
             return 0
+        
         # wenn neue schiffe gebaut wurden
-        if self.rl_state[self.rl_names.index("ships_in_shipyard")] > \
-             self.last_rl_state[self.rl_names.index("ships_in_shipyard")]:
+        # da der rl_state nach dem calc reward auch in den last_rl state geschrieben wird,
+        # ist die len bei dieser abfrage immer eins höher als wenn er schon integriert wurde.
+        if len(self.rl_state) > len(self.last_rl_state):
+            return 0 
+        
+        # TODO hier alle listen auf dictionaries umstellen 
+        if self.rl_state[idx][self.rl_names.index("ships_in_shipyard")] > \
+             self.last_rl_state[idx][self.rl_names.index("ships_in_shipyard")]:
              reward +=100
 
         # wenn schiffe auf resen geschickt werden und sie kore einsammeln
-        if self.rl_state[self.rl_names.index("kore_of_fleet")] > \
-             self.last_rl_state[self.rl_names.index("kore_of_fleet")]:
+        if self.rl_state[idx][self.rl_names.index("kore_of_fleet")] > \
+             self.last_rl_state[idx][self.rl_names.index("kore_of_fleet")]:
              reward +=15
              # TODO: hier das gegainte implementieren
 
-        if self.rl_state[self.rl_names.index("num_shipyards")] > \
-             self.last_rl_state[self.rl_names.index("num_shipyards")]: 
+        # if the number of own shipyards increases, there will be a new agent
+        new_shipyard_no = self.rl_state[idx][self.rl_names.index("num_shipyards")] - self.last_rl_state[idx][self.rl_names.index("num_shipyards")]
+        if new_shipyard_no >0: 
              print("new abse was build")
              reward +=500
 
+
         # reward if game ends:
-        if self.rl_state[self.rl_names.index("done")] == 1:
+        if self.rl_state[idx][self.rl_names.index("done")] == 1:
             print("Game is over.")
-            if self.rl_state[self.rl_names.index("n_opp_shipyards")] ==0 and self.rl_state[self.rl_names.index("num_shipyards")] >0:
+            if self.rl_state[idx][self.rl_names.index("n_opp_shipyards")] ==0 and self.rl_state[idx][self.rl_names.index("num_shipyards")] >0:
                 return float(reward + 100000)
-            if self.rl_state[self.rl_names.index("kore_left")]> self.rl_state[self.rl_names.index("kore_opp")]:
+            if self.rl_state[idx][self.rl_names.index("kore_left")]> self.rl_state[idx][self.rl_names.index("kore_opp")]:
                 reward +=100000
                 print(f"reward ist: {reward}")
-            elif self.rl_state[self.rl_names.index("kore_left")]< self.rl_state[self.rl_names.index("kore_opp")]:
+            elif self.rl_state[idx][self.rl_names.index("kore_left")]< self.rl_state[self.rl_names.index("kore_opp")]:
                 print("Game is lost")
                 reward -=100000
 
         return reward
         
 
-    def map_action(self, action_raw, obs, config, shipyard_idx):
+    def map_action(self, action_raw, obs, config, idx):
         board, me, turn, spawn_cost, kore_left, max_spawn, kore_opp, opp_shipyards, num_shipyards = unbundle_stuff(obs, config)
         
         # send ships to random direction
         if action_raw == 0:
             direction = Direction.random_direction()
-            shipcount = me.shipyards[shipyard_idx].ship_count
+            shipcount = me.shipyards[idx].ship_count
             if shipcount >0:
                 action = ShipyardAction.launch_fleet_with_flight_plan(shipcount ,direction.to_char())
             else: action = None
@@ -200,19 +239,21 @@ class Controller:
 
         # create new base
         elif action_raw == 2:
-            action = send_ships_to_create_new_base(shipyard=me.shipyards[shipyard_idx])
+            action = send_ships_to_create_new_base(shipyard=me.shipyards[idx])
 
         elif action_raw == 3:
-            action = attack_opponent_base(board, idx=shipyard_idx)
-            print("theoretically attacked opponent base")
-            if me.shipyards[0].ship_count >=50:
+            action = attack_opponent_base(board, idx=idx)
+            # print("theoretically attacked opponent base")
+            if me.shipyards[idx].ship_count >=50:
                 print("vermutlich auch angegriffen")
         return action
 
     def save_models(self):
-        self.agent.model.save(self.q_eval_model_file)
-        self.agent.target_model.save(self.q_target_model_file)
-        print('... saving models ...')
+        # TODO hier nochmal genau reinschauen, ist njoch nciht richtig implementiert
+        for agent in self.agents:
+            agent.model.save(self.q_eval_model_file)
+            agent.target_model.save(self.q_target_model_file)
+            print('... saving models ...')
 
 
 class Agent:
